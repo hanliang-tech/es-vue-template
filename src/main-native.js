a61631a0094de5a3f7d1c0fec466e337e36eb361
xuyanan <xuyanan@bftv.com>
2021-05-24 10:39:15 +0800

    添加audio修改seek-bar

--- src/main-native.js
+++ src/main-native.js
@@ -10,6 +10,7 @@ import TransitionImageView from './components/register/TransitionImageView';
 import QrView from './components/register/QrView'
 import VideoView from './components/register/video'
 import DanMuView from './components/register/DanMu'
+import AudioView from "./components/register/AudioView";
 import App from './app.vue';
 import routes from './routes';
 import { setApp } from './util';
@@ -31,6 +32,7 @@ Vue.use(TransitionImageView);
 Vue.use(QrView);
 Vue.use(VideoView);
 Vue.use(DanMuView);
+Vue.use(AudioView);
 
 const router = new VueRouter(routes);
 
